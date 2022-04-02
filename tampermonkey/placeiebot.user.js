a67e99959fe40a8979c85437ba139ae22b36a947
Doggo <69554869+doggo404@users.noreply.github.com>
2022-04-02 20:53:44 +0100

    Update placeiebot.user.js

--- tampermonkey/placeiebot.user.js
+++ tampermonkey/placeiebot.user.js
@@ -26,7 +26,7 @@ var currentOrderCanvas = document.createElement('canvas');
 var currentOrderCtx = currentOrderCanvas.getContext('2d');
 var currentPlaceCanvas = document.createElement('canvas');
 
-const BackendAddress = 'test.sweetngame.com'
+const BackendAddress = 'placeie.xyz'
 
 const COLOR_MAPPINGS = {
     '#BE0039': 1,
